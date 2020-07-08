46cdead69f58eabdc6631c3e7f2f445eaa0a1fa9
Yohane <2540546236@qq.com>
2020-07-09 02:35:02 +0800

    Update canvas2d-fequency1.ts

--- src/canvas2d-fequency1.js
+++ src/canvas2d-fequency1.js
@@ -298,6 +298,7 @@ var __spreadArrays = (this && this.__spreadArrays) || function () {
 //   ctx.stroke();
 // };
 // drawAudio('./src/assets/miku.mp3');
+var simplex = new SimplexNoise();
 onload = function () {
     var $canvas = document.querySelector('canvas');
     var c = $canvas.getContext('2d');
@@ -370,6 +371,14 @@ onload = function () {
     });
     circle.x = width * .5;
     circle.y = height * .5;
+    var getAvg = function (frequencyData) {
+        var value = 0;
+        var values = [].slice.call(frequencyData);
+        values.forEach(function (v) {
+            value += v;
+        });
+        return value / values.length;
+    };
     var $play = document.querySelector('#play');
     var $audio = document.querySelector('audio');
     var context = new (window.AudioContext || window.webkitAudioContext)();
@@ -388,13 +397,57 @@ onload = function () {
         // analyser.getByteFrequencyData(dataArray)
         canPlay = true;
     };
+    var lineStyle = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+    var lineStyle2 = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
     requestAnimationFrame(function animate() {
         requestAnimationFrame(animate);
         c.clearRect(0, 0, $canvas.width, $canvas.height);
-        circle.render(c);
+        //circle.render(c)
         if (canPlay) {
             analyser.getByteFrequencyData(dataArray);
-            circle.wave(dataArray);
+            var pointsNum_1 = 101;
+            drawClosedCurve({
+                showPoints: true,
+                points: __spreadArrays(Array(pointsNum_1)).map(function (v, i) {
+                    var frequencyData = dataArray.slice(0, dataArray.length * .5);
+                    var frAvg = Math.pow(getAvg(frequencyData) / 255, .3);
+                    var t = simplex.noise3D(Math.cos(i / (pointsNum_1) * Math.PI * 2), Math.sin(i / (pointsNum_1) * Math.PI * 2), frAvg * 2);
+                    var selfRadius = 140 + t * 20;
+                    var x = width * .5 + Math.cos(i / (pointsNum_1) * Math.PI * 2) * selfRadius;
+                    var y = height * .5 + Math.sin(i / (pointsNum_1) * Math.PI * 2) * selfRadius;
+                    return {
+                        x: x, y: y
+                    };
+                }),
+                ctx: c,
+                start: function (ctx) {
+                    ctx.shadowBlur = 10;
+                    ctx.shadowColor = 'rgba(255,255,255,.8)';
+                    ctx.lineWidth = 3;
+                    ctx.strokeStyle = lineStyle2;
+                }
+            });
+            drawClosedCurve({
+                showPoints: true,
+                points: __spreadArrays(Array(pointsNum_1)).map(function (v, i) {
+                    var frequencyData = dataArray.slice(0, dataArray.length * .5);
+                    var frAvg = Math.pow(getAvg(frequencyData) / 255, .5);
+                    var t = simplex.noise3D(Math.cos(i / (pointsNum_1) * Math.PI * 2), Math.sin(i / (pointsNum_1) * Math.PI * 2), frAvg * 2);
+                    var selfRadius = 150 + t * 20;
+                    var x = width * .5 + Math.cos(i / (pointsNum_1) * Math.PI * 2) * selfRadius;
+                    var y = height * .5 + Math.sin(i / (pointsNum_1) * Math.PI * 2) * selfRadius;
+                    return {
+                        x: x, y: y
+                    };
+                }),
+                ctx: c,
+                start: function (ctx) {
+                    ctx.shadowBlur = 10;
+                    ctx.shadowColor = 'rgba(255,255,255,.8)';
+                    ctx.lineWidth = 5;
+                    ctx.strokeStyle = lineStyle;
+                }
+            });
         }
     });
     // window.rad = 0
@@ -406,4 +459,26 @@ onload = function () {
     //   )+Math.PI*.5
     //   console.log(window.rad)
     // }
+    var drawClosedCurve = function (_a) {
+        var points = _a.points, start = _a.start, ctx = _a.ctx, showPoints = _a.showPoints;
+        var ctrlPoint = {};
+        var ctrlPoint1 = {};
+        ctrlPoint1.x = (points[0].x + points[points.length - 1].x) * .5;
+        ctrlPoint1.y = (points[0].y + points[points.length - 1].y) * .5;
+        ctx.save();
+        start(ctx);
+        ctx.beginPath();
+        ctx.moveTo(ctrlPoint1.x, ctrlPoint1.y);
+        for (var i = 0; i < points.length - 1; i++) {
+            ctrlPoint.x = (points[i].x + points[i + 1].x) / 2;
+            ctrlPoint.y = (points[i].y + points[i + 1].y) / 2;
+            ctx.quadraticCurveTo(points[i].x, points[i].y, ctrlPoint.x, ctrlPoint.y);
+            if (showPoints) {
+                c.fillRect(points[i].x, points[i].y, 3, 3);
+            }
+        }
+        c.quadraticCurveTo(points[points.length - 1].x, points[points.length - 1].y, ctrlPoint1.x, ctrlPoint1.y);
+        ctx.stroke();
+        ctx.restore();
+    };
 };
--- src/canvas2d-fequency1.ts
+++ src/canvas2d-fequency1.ts
@@ -324,6 +324,7 @@
 // drawAudio('./src/assets/miku.mp3');
 
 
+var simplex = new SimplexNoise()
 onload = ()=>{
   const $canvas:HTMLCanvasElement = document.querySelector('canvas')
   const c:CanvasRenderingContext2D = $canvas.getContext('2d') as CanvasRenderingContext2D
@@ -418,6 +419,14 @@ onload = ()=>{
   })
   circle.x = width*.5
   circle.y = height*.5
+  var getAvg = (frequencyData) => {
+    var value = 0
+    var values = [].slice.call(frequencyData)
+    values.forEach(function (v) {
+      value += v
+    })
+    return value / values.length
+  }
 
 
   const $play:HTMLDivElement = document.querySelector('#play')
@@ -445,18 +454,79 @@ onload = ()=>{
   
   }
 
+  const lineStyle = '#'+Math.floor(Math.random()*0xffffff).toString(16)
+  const lineStyle2 = '#'+Math.floor(Math.random()*0xffffff).toString(16)
 
   requestAnimationFrame(function animate(){
     requestAnimationFrame(animate)
   
     c.clearRect(0,0,$canvas.width,$canvas.height)
-    circle.render(c)
+    //circle.render(c)
 
     if(canPlay){
       
       analyser.getByteFrequencyData(dataArray)
 
-      circle.wave(dataArray)
+      const pointsNum = 101
+      drawClosedCurve({
+        showPoints: true,
+        points: [...Array(pointsNum)].map((v, i) => {
+
+          var frequencyData = dataArray.slice(0,dataArray.length*.5)
+        
+          var frAvg = Math.pow(getAvg(frequencyData)/255,.3)
+
+          let t = simplex.noise3D(
+            Math.cos(i / (pointsNum ) * Math.PI * 2),
+            Math.sin(i / (pointsNum ) * Math.PI * 2),
+            frAvg*2
+          )
+          let selfRadius = 140 + t*20
+          let x = width * .5 + Math.cos(i / (pointsNum ) * Math.PI * 2) * selfRadius
+          let y = height * .5 + Math.sin(i / (pointsNum ) * Math.PI * 2) * selfRadius
+
+          return {
+            x, y
+          }
+        }),
+        ctx: c,
+        start(ctx) {
+          ctx.shadowBlur = 10;
+          ctx.shadowColor = 'rgba(255,255,255,.8)'
+          ctx.lineWidth = 3
+          ctx.strokeStyle = lineStyle2
+        }
+      })
+
+      drawClosedCurve({
+        showPoints: true,
+        points: [...Array(pointsNum)].map((v, i) => {
+
+          var frequencyData = dataArray.slice(0,dataArray.length*.5)
+        
+          var frAvg = Math.pow(getAvg(frequencyData)/255,.5)
+
+          let t = simplex.noise3D(
+            Math.cos(i / (pointsNum ) * Math.PI * 2),
+            Math.sin(i / (pointsNum ) * Math.PI * 2),
+            frAvg*2
+          )
+          let selfRadius = 150 + t*20
+          let x = width * .5 + Math.cos(i / (pointsNum ) * Math.PI * 2) * selfRadius
+          let y = height * .5 + Math.sin(i / (pointsNum ) * Math.PI * 2) * selfRadius
+
+          return {
+            x, y
+          }
+        }),
+        ctx: c,
+        start(ctx) {
+          ctx.shadowBlur = 10;
+          ctx.shadowColor = 'rgba(255,255,255,.8)'
+          ctx.lineWidth = 5
+          ctx.strokeStyle = lineStyle
+        }
+      })
     }
   })
 
@@ -470,5 +540,27 @@ onload = ()=>{
 
   //   console.log(window.rad)
   // }
+  
+  
+    const drawClosedCurve = ({ points, start, ctx, showPoints }) => {
+        const ctrlPoint = {}
+        const ctrlPoint1 = {}
+        ctrlPoint1.x = (points[0].x + points[points.length - 1].x) * .5
+        ctrlPoint1.y = (points[0].y + points[points.length - 1].y) * .5
+        ctx.save()
+        start(ctx)
+        ctx.beginPath()
+        ctx.moveTo(ctrlPoint1.x, ctrlPoint1.y)
+        for (let i = 0; i < points.length - 1; i++) {
+          ctrlPoint.x = (points[i].x + points[i + 1].x) / 2
+          ctrlPoint.y = (points[i].y + points[i + 1].y) / 2
+          ctx.quadraticCurveTo(points[i].x, points[i].y, ctrlPoint.x, ctrlPoint.y)
 
-}
+          if (showPoints) {
+            c.fillRect(points[i].x, points[i].y, 3, 3)
+          }
+        }
+        c.quadraticCurveTo(points[points.length - 1].x, points[points.length - 1].y, ctrlPoint1.x, ctrlPoint1.y)
+        ctx.stroke()
+        ctx.restore()
+      }
